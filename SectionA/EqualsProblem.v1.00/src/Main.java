f568e5a22c57232c6c1dc5c87e739170746204f3
Larry Clement <lclement@calbaptist.edu>
2023-02-23 08:31:27 -0800

    Final PRIOR to Class Session

---
+++ SectionA/EqualsProblem.v1.00/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Point p1 = new Point(5,3);
+        Point p2 = new Point(5,3);
+        Point p3 = p2;
+
+        System.out.println("p1: " + p1.toString());
+        System.out.println("p2: " + p2.toString());
+        System.out.println("p3: " + p3.toString());
+
+        System.out.println("p1 == p2 " + (p1 == p2));
+        System.out.println("p1 == p3 " + (p1 == p3));
+        System.out.println("p3 == p2 " + (p3 == p2));
+//
+        System.out.println("p1.equals(p2) " + (p1.equals(p2)));
+        System.out.println("p1.equals(p3) " + (p1.equals(p3)));
+        System.out.println("p3.equals(p2) " + (p3.equals(p2)));
+//
+        System.out.println("Other: " + p1.equals("Hello World!"));
+    }
+}
